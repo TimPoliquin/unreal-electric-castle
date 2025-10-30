dd653a5921a1dff8331030edfec085a43aebb8b7
Tim Poliquin <tim.poliquin@gmail.com>
2025-10-30 15:18:03 -0400

    [fix] Mouse aiming screen fix (#36)
    - Aiming with the mouse now disables mouse movement, allowing for
    smoother, more consistent aiming

--- Source/ElectricCastle/Public/Player/ElectricCastlePlayerController.h
+++ Source/ElectricCastle/Public/Player/ElectricCastlePlayerController.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ec69923e66582f44983b7309ff309d45f8f46b948427f2086ca7fc10f4f9664
-size 5124
+oid sha256:bd2a4a10b64acf3c34b03f9b90cc4a5f5977c2b3ed2386d03a7435b73cef0acd
+size 5146
