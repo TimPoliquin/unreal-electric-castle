0764f92a328f9308316d0e2ee451f53b3a51acb7
Tim Poliquin <tim.poliquin@gmail.com>
2026-02-12 16:47:06 -0500

    [fix] Remove hack for common ui input handling
    closes https://github.com/TimPoliquin/unreal-electric-castle/issues/82
    
    - Remove hack to override Virtual_Accept key
    - Add ElectricCastleCommonUIActionRouter implementation to override VirtualAccept mouse button

--- Source/ElectricCastle/Private/Game/ElectricCastleGameInstance.cpp
+++ Source/ElectricCastle/Private/Game/ElectricCastleGameInstance.cpp
@@ -7,10 +7,6 @@
 
 UElectricCastleGameInstance::UElectricCastleGameInstance()
 {
-	// This is a hack to disable Virtual Accept
-	// Associated with FCommonAnalogCursor::HandleKeyDownEvent and FAnalogCursor::HandleKeyDownEvent
-	FKey& virtual_key = const_cast<FKey&>(EKeys::Virtual_Gamepad_Accept);
-	virtual_key = EKeys::Invalid;
 }
 
 UElectricCastleGameInstance* UElectricCastleGameInstance::Get(const UObject* WorldContextObject)
---
+++ Source/ElectricCastle/Public/Input/ElectricCastleCommonUIActionRouter.h
@@ -0,0 +1,30 @@
+﻿// Copyright Alien Shores
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Input/CommonUIActionRouterBase.h"
+#include "Input/CommonAnalogCursor.h"
+#include "ElectricCastleCommonUIActionRouter.generated.h"
+
+class FElectricCastleCommonAnalogCursor : public FCommonAnalogCursor
+{
+public:
+	explicit FElectricCastleCommonAnalogCursor(const UCommonUIActionRouterBase& InActionRouter) : FCommonAnalogCursor(InActionRouter)
+	{
+	}
+
+	virtual bool ShouldVirtualAcceptSimulateMouseButton(const FKeyEvent& InKeyEvent, EInputEvent InputEvent) const override;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class ELECTRICCASTLE_API UElectricCastleCommonUIActionRouter : public UCommonUIActionRouterBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual TSharedRef<FCommonAnalogCursor> MakeAnalogCursor() const override;
+};
